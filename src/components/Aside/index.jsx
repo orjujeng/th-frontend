b0279414fb5b94c5ae2a578be3aa8b0bc00cdcd6
orjujeng <orjujeng@hotmail.com>
2024-07-12 01:12:32 +0800

    aside done

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,38 +1,27 @@
-import React,{Fragment,useState} from 'react'
+import React, { Fragment, useState } from 'react'
 import './index.scss'
 import AsideList from '../AsideList'
 export default function Aside() {
-  const [asideContext,setAsideContext] = useState([
-    {title:null,context:[
-      {title:'Dashboard',context:null},
-      {title:'Analytics',context:null},
-      {title:'Alternative',context:null},
-      {title:'Inventory Management',context:null}]
+  const [asideContext, setAsideContext] = useState([
+    {
+      title: null, context: [
+        { title: 'Dashboard', context: null },
+        { title: 'Analytics', context: null },
+        { title: 'Alternative', context: null },
+        { title: 'Inventory Management', context: null }]
     },
-    {title:'PAGES',context:[
-      {title:'Customers',context:['Customer List','View Customer','New Customer']},
-      {title:'Products',context:['Products List','View Products','New Products']},
-      {title:'Orders',context:['Order List','View Order']},
-      {title:'Help Center',context:['FAQ 1','FAQ 2']},
-      {title:'Pricing',context:['Pricing 1','Pricing 2']},
-      {title:'User List',context:['User List 1','User List 2','User List 3','User List 4']},
-      {title:'Forms',context:['Order Form','Invoice Form','Property Listing Form','Basic','Upload','Wizard']},
-      {title:'Matx List',context:['List']},
-      {title:'Session/Auth',context:['Sign in','Sign up','Forgot Password','Error']},
-      {title:'Left Sidebar Car',context:null}
-      ]
-    },
-    {title:'PAGES',context:[
-      {title:'Customers',context:['Customer List','View Customer','New Customer']},
-      {title:'Products',context:['Products List','View Products','New Products']},
-      {title:'Orders',context:['Order List','View Order']},
-      {title:'Help Center',context:['FAQ 1','FAQ 2']},
-      {title:'Pricing',context:['Pricing 1','Pricing 2']},
-      {title:'User List',context:['User List 1','User List 2','User List 3','User List 4']},
-      {title:'Forms',context:['Order Form','Invoice Form','Property Listing Form','Basic','Upload','Wizard']},
-      {title:'Matx List',context:['List']},
-      {title:'Session/Auth',context:['Sign in','Sign up','Forgot Password','Error']},
-      {title:'Left Sidebar Car',context:null}
+    {
+      title: 'PAGES', context: [
+        { title: 'Customers', context: ['Customer List', 'View Customer', 'New Customer'] },
+        { title: 'Products', context: ['Products List', 'View Products', 'New Products'] },
+        { title: 'Orders', context: ['Order List', 'View Order'] },
+        { title: 'Help Center', context: ['FAQ 1', 'FAQ 2'] },
+        { title: 'Pricing', context: ['Pricing 1', 'Pricing 2'] },
+        { title: 'User List', context: ['User List 1', 'User List 2', 'User List 3', 'User List 4'] },
+        { title: 'Forms', context: ['Order Form', 'Invoice Form', 'Property Listing Form', 'Basic', 'Upload', 'Wizard'] },
+        { title: 'Matx List', context: ['List'] },
+        { title: 'Session/Auth', context: ['Sign in', 'Sign up', 'Forgot Password', 'Error'] },
+        { title: 'Left Sidebar Car', context: null }
       ]
     }
   ])
@@ -44,8 +33,8 @@ export default function Aside() {
           <span className='asideTh'>Timesheet</span>
         </div>
         <div className="asideContext">
-          {asideContext.map((item)=>{
-            return <AsideList title={item.title} context ={item.context}/>
+          {asideContext.map((item) => {
+            return <AsideList title={item.title} context={item.context} />
           })}
         </div>
       </div>
--- src/components/AsideItem/index.jsx
+++ src/components/AsideItem/index.jsx
@@ -1,17 +1,36 @@
-import React,{Fragment} from 'react'
+import React, { Fragment } from 'react'
 import './index.scss'
 export default function AsideItem(props) {
-  React.useEffect(()=>{
-    console.log((props.context==null || props.context.length== 0))
-},[])
+  React.useEffect(() => {
+    console.log((props.context))
+  }, [])
+  const arrowRef =React.useRef();
+
+  const asideSubItemRef =React.useRef();
+  const colArrow=()=>{
+    if(arrowRef.current.style.rotate=='90deg'){
+      arrowRef.current.style.rotate='0deg'
+      asideSubItemRef.current.style.display='none'
+    }else{
+      arrowRef.current.style.rotate='90deg'
+      asideSubItemRef.current.style.display='block'
+    }
+  }
   return (
     <Fragment>
-      <div className="asideItem">
-        <div className="asideTitle">
-          <span class="iconfont">&#xe76a;</span>
+      <div className="asideItems">
+      <div className="asideItem" onClick={colArrow}>
+        <span class="iconfont asideItemTitleIcon">&#xe76a;</span>
+        <span className="asideTitle">
           {props.title}
-          <span class="iconfont" style={{ display: (props.context==null || props.context.length== 0)? 'none':'display' }} >&#xe76a;</span>
-        </div>
+        </span>
+        <span class="iconfont asideItemTitleRowArrow" style={{ display: (props.context == null || props.context.length == 0) ? 'none' : 'display' }} ref={arrowRef}>&#xe63c;</span> 
+      </div>
+      <div className="asideSubItem" ref={asideSubItemRef} style={{ display: 'none' }}>
+        {props.context!=null?props.context.map((items)=>{
+          return <div className="asideSubItemTitle">{items}</div>;
+        }):null}
+      </div>
       </div>
     </Fragment>
   )
