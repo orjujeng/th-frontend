c8cbb55d720d94d7def6fa063e6615b106202efa
orjujeng <orjujeng@hotmail.com>
2024-07-23 00:54:17 +0800

    to chart

--- src/components/DashboradUI/index.jsx
+++ src/components/DashboradUI/index.jsx
@@ -2,6 +2,13 @@ import React, { Fragment } from 'react'
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import './index.scss'
+import Paper from '@mui/material/Paper';
+import PeopleAltSharpIcon from '@mui/icons-material/PeopleAltSharp';
+import SupportSharpIcon from '@mui/icons-material/SupportSharp';
+import AccountTreeSharpIcon from '@mui/icons-material/AccountTreeSharp';
+import PunchClockSharpIcon from '@mui/icons-material/PunchClockSharp';
+import Box from '@mui/material/Box';
+
 export default function DashboardUI() {
   const [selectPeriod, setSelectPeriod] = React.useState('202407');
 
@@ -36,6 +43,66 @@ export default function DashboardUI() {
               <MenuItem value="202307">Last Year</MenuItem>
             </Select>
           </div>
+        </div>
+        <div className="dashboradMemberNumber">
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'nowrap',
+              justifyContent:'space-between',
+              '& > :not(style)': {
+                m: 1,
+                width: '24%',
+                height: 105,
+              },
+            }}
+          >
+            <Paper elevation={3} sx={{
+              display: 'flex',
+              justifyContent:'start',
+              padding:'20px'
+            }}>
+              <div className="dashboardIcon"><PeopleAltSharpIcon/></div>
+              <div className="dashboradPaperCommon">
+                <div className="dashboradNumber">10,495</div>
+                <div className="dashboradTitle">New Members</div>
+              </div>
+            </Paper>
+            <Paper elevation={3} sx={{
+              display: 'flex',
+              justifyContent:'start',
+              padding:'20px',
+              alignItems: 'center'
+            }}>
+              <div className="dashboardIcon"><SupportSharpIcon/></div>
+              <div className="dashboradPaperCommon">
+                <div className="dashboradNumber">45,269</div>
+                <div className="dashboradTitle">Support Members</div>
+              </div>
+            </Paper>
+            <Paper elevation={3} sx={{
+              display: 'flex',
+              justifyContent:'start',
+              padding:'20px'
+            }}>
+              <div className="dashboardIcon"><AccountTreeSharpIcon/></div>
+              <div className="dashboradPaperCommon">
+                <div className="dashboradNumber">20,965</div>
+                <div className="dashboradTitle">Support Projects</div>
+              </div>
+            </Paper>
+            <Paper elevation={3} sx={{
+              display: 'flex',
+              justifyContent:'start',
+              padding:'20px'
+            }}>
+              <div className="dashboardIcon"><PunchClockSharpIcon/></div>
+              <div className="dashboradPaperCommon">
+                <div className="dashboradNumber">20,965</div>
+                <div className="dashboradTitle">Timesheet Collection</div>
+              </div>
+            </Paper>
+          </Box>
         </div>
       </div>
     </Fragment>
--- src/pages/Main/Dashboard/index.jsx
+++ src/pages/Main/Dashboard/index.jsx
@@ -1,9 +1,12 @@
 import React,{Fragment}from 'react'
 import DashboardUI from '../../../components/DashboradUI'
+import './index.scss'
 export default function Dashboard() {
   return (
     <Fragment>
+      <div className="dashboardWarp">
       <DashboardUI/>
+      </div>
     </Fragment>
   )
 }
