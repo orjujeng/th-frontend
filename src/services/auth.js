5763cc9c6203975cfeea7e949ce29c94ad477269
orjujeng <orjujeng@hotmail.com>
2024-10-08 01:22:57 +0800

    change to dev url

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
 import {HttpsGet,HttpsPost} from '../utils/axiosUtils'
 
-export const loginService = (param)=>{ return HttpsGet('https://poc.orjujeng.click/api/auth/login',param)}
-export const resetService = (param)=>{ return HttpsPost('https://poc.orjujeng.click/api/auth/reset',param)}
-export const regService = (param)=>{ return HttpsPost('https://poc.orjujeng.click/api/auth/reg',param)}
+export const loginService = (param)=>{ return HttpsGet('https://th-dev.orjujeng.click/api/auth/login',param)}
+export const resetService = (param)=>{ return HttpsPost('https://th-dev.orjujeng.click/api/auth/reset',param)}
+export const regService = (param)=>{ return HttpsPost('https://th-dev.orjujeng.click/api/auth/reg',param)}
--- src/services/dashborad.js
+++ src/services/dashborad.js
@@ -1,3 +1,3 @@
 import {HttpsGet,HttpsPost} from '../utils/axiosUtils'
 
-export const dashboradService = (param)=>{ return HttpsPost('https://poc.orjujeng.click/api/dashborad/dashborad',param)}
+export const dashboradService = (param)=>{ return HttpsPost('https://th-dev.orjujeng.click/api/dashborad/dashborad',param)}
